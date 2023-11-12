fef29e224e76c09e812cec9a02114b7c9051e055
Bardakis <basilhsbar5@gmail.com>
2023-11-12 20:13:02 +0200

    Άλλαξα λιγο το database, μετέτρεψα το όνομα απο admin σε accounts και έβαλα άλλο ενα πεδίο type το οποίο δείχνει τι χρήστης είναι, 0 για admin 1 για πολίτης μεχρι τώρα. Πιά στα / και /admin και /home (/home είναι του πολίτη το Page) τεστάρει άν έχει το σωστό account type ο χρήστης, αν όχι τότε τον πηγαίνει στο /auth το οποίο τον κάνει redirect στο σωστό page.

--- index.js
+++ index.js
@@ -49,7 +49,7 @@ app.post('/', (req, res) => {
 
 	if (username && password) {
 		// Execute SQL query that'll select the account from the database based on the specified username and password
-		db.query('SELECT * FROM admin WHERE username = ? AND password = ?', [username, password], function(error, results, fields) {
+		db.query('SELECT type FROM accounts WHERE username = ? AND password = ?', [username, password], function(error, results, fields) {  //Get specific account type
 			// If there is an issue with the query, output the error
 			if (error) throw error;
 			// If the account exists
@@ -57,8 +57,10 @@ app.post('/', (req, res) => {
 				// Authenticate the user
 				req.session.loggedin = true;
 				req.session.username = username;
-				// Redirect to home page
-				res.redirect('/home');
+				req.session.type = results[0].type;		//Give the account type to the session variable
+				// Direct to auth page
+				res.redirect('/auth');
+
 			} else {
 				res.send('Incorrect Username and/or Password, please try again!');
 			}			
@@ -70,17 +72,86 @@ app.post('/', (req, res) => {
 	}
 });
 
-app.get('/home', (req, response) => {
-    let sql = 'SELECT * FROM admin';
+
+app.get('/register', function (req, res) {
+	// Render login template
+	if (req.session.loggedin) {
+		res.redirect('/auth');      //If logged in, redirect to auth
+	}
+	else {
+		res.sendFile(path.join(__dirname + '/register.html'));   //If not logged in, showlogin page
+	}
+});
+
+// auth route
+app.post('/register', (req, res) => {
+	let username = req.body.username;
+	let password = req.body.password;
+
+	if (username && password) {
+		// Execute SQL query that'll register the account to the database
+		db.query('SELECT * FROM accounts WHERE username = ?', [username], function (error, results, fields) {
+			// If there is an issue with the query, output the error
+			if (error) throw error;
+			// Account already exists
+			if (results.length > 0) {
+				response.send('Username already exists, please pick a different one.')
+			} else {
+				db.query('INSERT INTO accounts VALUES (?,?,1)', [username,password], function (error, results, fields) {
+					if (error) throw error;
+				});
+				res.redirect('/');
+
+			}
+			res.end();
+		});
+	} else {
+		res.send('Please enter Username and Password!');
+		res.end();
+	}
+});
+
+
+app.get('/home', (req, res) => {
+    let sql = 'SELECT * FROM accounts';
     db.query(sql, (err, result) => {
         if (err) throw err;
-		if (req.session.loggedin) {
+		if (req.session.type==1) {			//Checking to see if user is a citizen
 			
 			response.send('Welcome back, ' + req.session.username);
 		}
-		else { response.send('Please log in to view this page') }
+		else { res.redirect('/auth'); }    //If not, redirect to the right page
 			
     });
+});
+
+
+app.get('/admin', (req, res) => {
+	let sql = 'SELECT * FROM accounts';
+	db.query(sql, (err, result) => {
+		if (err) throw err;
+		if (req.session.type == 0) {        //Checking to see if user is an admin
+
+			res.sendFile(path.join(__dirname + '/admin.html')); 
+		}
+		else { res.redirect('/auth'); }    //If not, redirect to the right page
+
+	});
+});
+
+app.get('/auth', (req, res) => {    //Pages go through /auth to see what permissions the user has and point them to the right page
+	let sql = 'SELECT * FROM accounts';
+	db.query(sql, (err, result) => {
+		if (err) throw err;
+		if (req.session.loggedin) {
+			if (req.session.type == 0) { res.redirect('/admin'); }
+			if (req.session.type == 1) { res.redirect('/home'); }
+
+			//response.send('Welcome back, ' + req.session.username);
+		}
+		else { res.redirect('/'); }
+
+	});
 });
 
 
