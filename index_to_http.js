23703eeca5deec6b894a453a6b89f32c8e314dc9
plat <teoplatonas2002@gmail.com>
2023-11-16 17:56:36 +0200

    //cookies and type cases //error : cannot write headers after they are sent to the client

--- index_to_http.js
+++ index_to_http.js
@@ -2,8 +2,8 @@
 const http = require('http');
 const mysql = require('mysql')
 const path = require('path');
-const methodOverride = require('method-override');
 const fs = require('fs');
+const { parse } = require('cookie');
 
 // Establishing connection
 const db = mysql.createConnection({
@@ -18,10 +18,17 @@ db.connect((err) => {
     else { console.log('MySql Connected'); }
 });
 
+// Use cookie middleware
+const parseCookies = (request) => {
+    const rawCookies = request.headers.cookie || '';
+    return parse(rawCookies);
+};
+
+
 // GET for rendering pages,
 // POST for user actions
-
 const server = http.createServer((request, response) => {
+
 	let urlPath = request.url;
 	// to make ext extraction work
 	if (urlPath === '/') {
@@ -33,7 +40,7 @@ const server = http.createServer((request, response) => {
 		urlPath += '.html';
 	}
 	const extname = String(path.extname(urlPath)).toLowerCase();
-	
+    
 	const mimeTypes = {
 		'.html': 'text/html',
 		'.css': 'text/css',
@@ -45,7 +52,7 @@ const server = http.createServer((request, response) => {
 		'.svg': 'image/svg+xml',
 		'.ico': '/image/ico'
 	};
-
+	
 	let imgTypes = [
 		'.png',
 		'.jpg',
@@ -53,82 +60,173 @@ const server = http.createServer((request, response) => {
 		'.svg',
 		'.ico'
 	];
-	
+
 	let contentType = 'text/html';
 	contentType = mimeTypes[extname];
-	
+	// Parse cookies
+    const cookies = parseCookies(request);
+
+	 // Handle routes related to cookies
+	  if (urlPath === '/setcookie') {
+        // Set a cookie named 'myCookie'
+        const cookieValue = 'Hello, Cookies!';
+        response.setHeader('Set-Cookie', `myCookie=${cookieValue}; Max-Age=900000; HttpOnly`);
+        response.writeHead(200, { 'Content-Type': 'text/plain' });
+        response.end('Cookie has been set!');
+    } else if (urlPath === '/getcookie') {
+        // Read the value of the 'myCookie' cookie
+        const myCookieValue = cookies.myCookie;
+
+        if (myCookieValue) {
+            response.writeHead(200, { 'Content-Type': 'text/plain' });
+            response.end(`Value of myCookie: ${myCookieValue}`);
+        } else {
+            response.writeHead(200, { 'Content-Type': 'text/plain' });
+            response.end('myCookie not found.');
+        }
+    } else if (urlPath === '/clearcookie') {
+        // Clear the 'myCookie' cookie
+        response.setHeader('Set-Cookie', `myCookie=; Max-Age=0`);
+        response.writeHead(200, { 'Content-Type': 'text/plain' });
+        response.end('Cookie has been cleared!');
+    } else {
 	// TODO: GET/POST first, URL second. Can change if need be
-	if (request.method === 'GET') {
-		console.log(`GET: ${urlPath}`);
-		if (extname === '.html') {
-			let filePath;
-			switch (request.url) {
-				case '/':
-					filePath = path.join(__dirname, 'views', 'login.html');
-					//TODO: Add if session
-					
-					fs.readFile(filePath, 'utf8', (err, data) => {
-						if (err) {
-							response.writeHead(500, { 'Content-Type': 'text/plain' });
-							response.end('Internal Server Error');
-						} else {
-							response.writeHead(200, { 'Content-Type': contentType });
-							response.end(data, 'utf-8');
-						}
-						});
-						break;
-						
-				case '/login':
-				case '/register':
-					filePath = path.join(__dirname, urlPath)
-					fs.readFile(filePath, 'utf8', (err, data) => {
-						if (err) {
-							response.writeHead(500, { 'Content-Type': 'text/plain' });
-							response.end('Internal Server Error');
-						} else {
-							response.writeHead(200, { 'Content-Type': contentType });
-							response.end(data, 'utf-8');
-						}
-						});
-						break;
+	    if (request.method === 'GET') {
+		    console.log(`GET: ${urlPath}`);
 
-				default:
-					response.writeHead(404, { 'Content-Type': 'text/plain' });
-					response.end('Not Found');
+			const myCookieValue = cookies.myCookie;
 
-			}
-		// Anything other than .html
-		} else {
-			let encoding = 'utf-8';
+			if (myCookieValue) {
+                // If the cookie exists, query the database to get the user type
+                db.query('SELECT type FROM accounts WHERE username = ?', [myCookieValue], function (error, results, fields) {
+                    if (error) throw error;
 
-			if (imgTypes.includes(extname)) {
-				encoding = 'binary';
-			}
+                    if (results.length > 0) {
+                        const userType = results[0].type;
 
-			const filePath = path.join(__dirname, urlPath);
-			console.log(`from ${filePath}`);
-
-			fs.readFile(filePath, encoding, (err, data) => {
-				if (err) {
-					response.writeHead(500, { 'Content-Type': 'text/plain' });
-					response.end('Internal Server Error');
-				} else {
-					response.writeHead(200, { 'Content-Type': contentType });
-					response.end(data, encoding);
-				}
-			});
-		}
-	} else if (request.method === 'POST') {
-		console.log(`POST: ${urlPath}`);
-		let body = '';
-
-
-		switch (urlPath) {
-			case '/register':
-				// fetch the request
-				request.on('data', (chunk) => {
-					body += chunk;
+                        // Redirect based on the user type
+						// admin 0,civilian 1,rescuer 2
+                        switch (userType) {
+                            case 0:
+                                response.writeHead(302, { 'Location': '/auth/admin-dashboard' });
+                                response.end();
+                                break;
+                            case 1:
+                                response.writeHead(302, { 'Location': '/auth/civilian-dashboard' });
+                                response.end();
+                                break;
+                            case 2:
+                                response.writeHead(302, { 'Location': '/auth/rescuer-dashboard' });
+                                response.end();
+                                break;
+                            default:
+                                // Handle other user types if needed
+                                response.writeHead(302, { 'Location': '/auth/default-dashboard' });
+                                response.end();
+                                break;
+                        }
+                    } else {
+                        // Handle the case when user data is not found
+                        response.writeHead(302, { 'Location': '/login' });
+                        response.end();
+                    }
 				});
+			} else {
+                // If the cookie doesn't exist, continue with your existing GET request handling logic
+                // For example, render the login page
+                const filePath = path.join(__dirname, 'views', 'login.html');
+                fs.readFile(filePath, 'utf8', (err, data) => {
+                    if (err) {
+                        response.writeHead(500, { 'Content-Type': 'text/plain' });
+                        response.end('Internal Server Error');
+                    } else {
+                        response.writeHead(200, { 'Content-Type': 'text/html' });
+                        response.end(data, 'utf-8');
+                    }
+                });
+            }
+		    if (extname === '.html') {
+			    let filePath;
+			    switch (request.url) {
+				    case '/':
+					    filePath = path.join(__dirname, 'views', 'login.html');
+					    //TODO: Add if session
+					
+					    fs.readFile(filePath, 'utf8', (err, data) => {
+						    if (err) {
+							    response.writeHead(500, { 'Content-Type': 'text/plain' });
+							    response.end('Internal Server Error');
+						    } else {
+							    response.writeHead(200, { 'Content-Type': contentType });
+							    response.end(data, 'utf-8');
+						    }
+						    });
+						    break;
+						
+				    case '/login':
+				    case '/register':
+					    filePath = path.join(__dirname, '/views', urlPath)
+					    fs.readFile(filePath, 'utf8', (err, data) => {
+						if (err) {
+							response.writeHead(500, { 'Content-Type': 'text/plain' });
+							response.end('Internal Server Error');
+						} else {
+							response.writeHead(200, { 'Content-Type': contentType });
+							response.end(data, 'utf-8');
+						}
+						});
+					break;
+
+					case '/auth':
+					let sql = 'SELECT type FROM accounts';
+					db.query(sql, (err, result) => {
+						if (err) throw err;
+						if (req.session.loggedin) {
+							if (req.session.type == 0) { res.redirect('/admin'); }
+							if (req.session.type == 1) { res.redirect('/home'); }
+				
+							//res.send('Welcome back, ' + req.session.username);
+						}
+						else { res.redirect('/'); }
+					});
+
+				    default:
+					    response.writeHead(404, { 'Content-Type': 'text/plain' });
+					    response.end('Not Found');
+
+			    }
+		    // Anything other than .html
+		    } else {
+			    let encoding = 'utf-8';
+
+			    if (imgTypes.includes(extname)) {
+				    encoding = 'binary';
+			    }
+
+			    const filePath = path.join(__dirname, urlPath);
+			    console.log(`from ${filePath}`);
+
+			    fs.readFile(filePath, encoding, (err, data) => {
+				    if (err) {
+					    response.writeHead(500, { 'Content-Type': 'text/plain' });
+					    response.end('Internal Server Error');
+				    } else {
+					    response.writeHead(200, { 'Content-Type': contentType });
+					    response.end(data, encoding);
+				    }
+			    });
+	      	}
+	    } else if (request.method === 'POST') {
+		    console.log(`POST: ${urlPath}`);
+		    let body = '';
+
+
+	       	switch (urlPath) {
+	     		case '/register':
+		   		// fetch the request
+		  		request.on('data', (chunk) => {
+					body += chunk;
+		 		});
 		
 				request.on('end', () => {
 					// Parse as JSON
@@ -164,11 +262,11 @@ const server = http.createServer((request, response) => {
 				}
 				});
 				break;
-		
-			default:
-				response.writeHead(404, { 'Content-Type': 'text/plain' });
-				response.end('Not Found');
 
+		    }
+	    } else {
+			response.writeHead(405, { 'Content-Type': 'text/plain' });
+            response.end('Method Not Allowed');
 		}
 	}
 });
