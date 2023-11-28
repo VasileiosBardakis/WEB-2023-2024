8359e99b2344bab354dde300f88d83c0e2d4370c
plat <teoplatonas2002@gmail.com>
2023-11-28 18:14:54 +0200

    mySQL test 2nd try

--- notes_setup/Database_Setup.sql
+++ notes_setup/Database_Setup.sql
@@ -1,4 +1,4 @@
-ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY 'root';
+cargoDeliveredALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY 'root';
 flush privileges;
 
 DROP DATABASE IF EXISTS saviors;
@@ -14,9 +14,14 @@ CREATE TABLE accounts (
 );
 
 INSERT INTO accounts VALUES
-('admin','admin',0, null, null),
-('test_admin', 'zoowee', 0, null, null),
-('npc', 'npc', 1, null, null),
+('admin','admin',0, null, null);
+INSERT INTO accounts VALUES
+('test_admin', 'zoowee', 0, null, null);
+INSERT INTO accounts VALUES
+('npc', 'npc', 1, null, null);
+INSERT INTO accounts VALUES
+('res', 'res', 2, null, null);
+INSERT INTO accounts VALUES
 ('mister_helper', 'forfree', 2, null, null);
 
 
@@ -44,7 +49,6 @@ CREATE TABLE announce (
     title VARCHAR(255),
     descr VARCHAR(255),
     items JSON
-    -- date_announced DATETIME default now()
 );
 
 
@@ -63,19 +67,77 @@ CREATE TABLE requests (
     date_requested DATETIME default now(),
     date_accepted DATETIME,
     date_completed DATETIME,
-    FOREIGN KEY (username) REFERENCES accounts(username)
-    -- FOREIGN KEY (item_id) REFERENCES items(id)
-    -- FK item gets annoying when flushing db
+    FOREIGN KEY (username) REFERENCES accounts(username),
+    FOREIGN KEY (item_id) REFERENCES items(id)
 );
 
-CREATE TABLE request_status_code (
-    status INT UNSIGNED PRIMARY KEY,
-    meaning VARCHAR(30) NOT NULL
-);
-INSERT INTO request_status_code VALUES
-(0, 'Pending'),
-(1, 'Accepted'),
-(2, 'Completed');
+CREATE TABLE `cargo` (
+  `vehicle` enum('A','B','C') NOT NULL,
+  `item_id` int NOT NULL,
+  `item_name` varchar(255) DEFAULT NULL,
+  `item_category` int DEFAULT NULL,
+  PRIMARY KEY (`vehicle`),
+  KEY `VEHICLE_ITEMS` (`item_id`),
+  CONSTRAINT `VEHICLE_ITEMS` FOREIGN KEY (`item_id`) REFERENCES `items` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+); 
+
+DROP PROCEDURE IF EXISTS cargoLoaded;
+
+DELIMITER $$
+
+CREATE PROCEDURE cargoLoaded(IN item_tl_id INT,IN vehicle_tl ENUM('A','B','C'))
+BEGIN 
+   
+   DECLARE tempItem_name VARCHAR(255);
+   DECLARE tempItem_category INT;
+
+   SELECT name INTO tempItem_name
+   FROM items WHERE id = item_tl_id; 
+
+   SELECT category INTO tempItem_category
+   FROM items WHERE id = item_tl_id;  
+   
+   IF(tempItem_name IS NULL OR tempItem_category IS NULL) THEN
+   INSERT INTO cargo 
+   VALUES (vehicle_tl, item_tl_id, tempItem_name, tempItem_category);
+   
+   DELETE FROM items where id=item_tl_id;
+   ELSE 
+   SIGNAL SQLSTATE VALUE '45000'
+   SET MESSAGE_TEXT = 'Item is out of stock';
+   END IF;
+
+END $$
+DELIMITER ;
+
+DROP PROCEDURE IF EXISTS cargoDelivered;
+
+DELIMITER $$
+
+CREATE PROCEDURE cargoDelivered(IN item_td_id INT,IN vehicle_td ENUM('A','B','C'))
+BEGIN 
+   
+   DECLARE tempItem_name VARCHAR(255);
+   DECLARE tempItem_category INT;
+
+   SELECT item_name INTO tempItem_name
+   FROM cargo WHERE item_id = item_td_id AND vehicle = vehicle_td; 
+
+   SELECT item_category INTO tempItem_category
+   FROM cargo WHERE item_id = item_td_id AND vehicle = vehicle_td;   
+   
+   IF(tempItem_name IS NULL OR tempItem_category IS NULL) THEN
+   INSERT INTO items 
+   VALUES (item_td_id, tempItem_name, tempItem_category);
+   
+   DELETE FROM cargo where id=item_tl_id AND vehicle = vehicle_td; 
+   ELSE 
+   SIGNAL SQLSTATE VALUE '45000'
+   SET MESSAGE_TEXT = 'Cargo is missing';
+   END IF;
+
+END $$
+DELIMITER ;
 
 SELECT 'requests' AS '';
 -- \! echo 'some text';
